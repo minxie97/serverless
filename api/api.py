fcdba73973f7b720e67ba40783e09f6a93a6548d
Minhang Xie <minxie97@gmail.com>
2022-02-02 18:41:47 -0800

    poke api

---
+++ api/api.py
@@ -0,0 +1,24 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        url_path = self.path
+        url_components = parse.urlsplit(url_path)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+
+        if "pokemon" in dic:
+            url = "https://pokeapi.co/api/v2/pokemon"
+            r = requests.get(url + dic)
+            data = r.json()
+            print(data)
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(str(data).encode())
+        
+        return
